13fe94e2524e0d53a5562d6400117e257e7346f2
Svetoslav Busarov <103368248+Buzzter99@users.noreply.github.com>
2024-02-24 14:49:12 +0200

    Add project files.

---
+++ BusarovsQuckBite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusarovsQuckBite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
