b7c36631fb75e7ba765af25fd805b94616dc868f
Svetoslav Busarov <103368248+Buzzter99@users.noreply.github.com>
2024-04-10 01:59:38 +0300

    add unit tests for custom sign in manager

---
+++ BusarovsQuickBite.Tests/FakeManager/FakeSignInManager.cs
@@ -0,0 +1,36 @@
+﻿using BusarovsQuckBite.Data.Models;
+using BusarovsQuckBite.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+namespace BusarovsQuickBite.Tests.FakeManager
+{
+    public class FakeManager<T> where T : ApplicationUser
+    {
+        public static ApplicationSignInManager<T> CreateMockSignInManager()
+        {
+            var userManagerMock = new Mock<UserManager<T>>(
+                Mock.Of<IUserStore<T>>(),
+                null, null, null, null, null, null, null, null);
+
+            var contextAccessorMock = new Mock<IHttpContextAccessor>();
+            var claimsFactoryMock = new Mock<IUserClaimsPrincipalFactory<T>>();
+            var optionsAccessorMock = new Mock<IOptions<IdentityOptions>>();
+            var loggerMock = new Mock<ILogger<SignInManager<T>>>();
+            var schemesMock = new Mock<IAuthenticationSchemeProvider>();
+            var confirmationMock = new Mock<IUserConfirmation<T>>();
+            var signInManager = new ApplicationSignInManager<T>(
+                userManagerMock.Object,
+                contextAccessorMock.Object,
+                claimsFactoryMock.Object,
+                optionsAccessorMock.Object,
+                loggerMock.Object,
+                schemesMock.Object,
+                confirmationMock.Object);
+            return signInManager;
+        }
+    }
+}
---
+++ BusarovsQuickBite.Tests/SignInManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using BusarovsQuckBite.Data.Models;
+using BusarovsQuckBite.Services;
+using BusarovsQuickBite.Tests.FakeManager;
+namespace BusarovsQuickBite.Tests
+{
+    [TestFixture]
+    public class SignInManagerTests
+    {
+        private readonly ApplicationSignInManager<ApplicationUser> _signInManager = FakeManager<ApplicationUser>.CreateMockSignInManager();
+        private readonly ApplicationUser _user = new();
+
+        [Test]
+        public async Task TestActiveUser()
+        {
+            _user.IsActive = true;
+            var canSignIn = await _signInManager.CanSignInAsync(_user);
+            Assert.IsTrue(canSignIn);
+        }
+        [Test]
+        public async Task TestInActiveUser()
+        {
+            _user.IsActive = false;
+            var canSignIn = await _signInManager.CanSignInAsync(_user);
+            Assert.IsFalse(canSignIn);
+        }
+    }
+}
