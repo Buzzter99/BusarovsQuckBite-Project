c1cfc0c8e8f9b8c13d9731d3f7aaeebe6f55433a
Svetoslav Busarov <103368248+Buzzter99@users.noreply.github.com>
2024-04-24 20:29:21 +0300

    use swagger in production as well as in developement

--- BusarovsQuckBite.Api/Program.cs
+++ BusarovsQuckBite.Api/Program.cs
@@ -9,13 +9,9 @@ namespace BusarovsQuckBite.Api
             builder.Services.AddAuthorization();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapGet("api/ExportBaseImg", () =>
